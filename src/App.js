fd74d648c7fbab3bc9919aa0c4a0ba557ae09218
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-05-26 12:14:52 -0700

    Got a rendering adjustable grid, and the ability to select

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Gamebox from './components/Gamebox'
 import './App.css';
 
 function App() {
+    const [generations, setGenerations] = useState(0)
+    const [columns, setColumns] = useState(25)
+    const [rows, setRows] = useState(25)
+    const [boxGrid, setBoxGrid] = useState(Array(rows).fill().map(() => Array(columns).fill(false)))
+
+    const selectBox = (row, col) => {
+        let gridCopy = JSON.parse(JSON.stringify(boxGrid))
+        gridCopy[row][col] = !gridCopy[row][col];
+        setBoxGrid(gridCopy)
+    }
+
   return (
     <div>
-        Test
+        <h1>Game of Life</h1>
+        <Gamebox rows={rows} columns={columns} boxFull={boxGrid} selectBox={selectBox}/>
+        <h2>{generations}</h2>
     </div>
   );
 }
