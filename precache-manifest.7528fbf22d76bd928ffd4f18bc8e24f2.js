3baaad355d918f7e36a029411b113418ce88d7de
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-05-28 16:32:54 -0700

    Updates

---
+++ precache-manifest.7528fbf22d76bd928ffd4f18bc8e24f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d7025b018d9a95e3da0ec2c76fd211",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a75e1b93c87a6df6b01",
+    "url": "/static/css/main.f08bca9e.chunk.css"
+  },
+  {
+    "revision": "5587c1bbbd783f878d02",
+    "url": "/static/js/2.1d9e5422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d9e5422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a75e1b93c87a6df6b01",
+    "url": "/static/js/main.ccb639cb.chunk.js"
+  },
+  {
+    "revision": "682e28584bbcafd2c9dd",
+    "url": "/static/js/runtime-main.6d0de52a.js"
+  }
+]);
